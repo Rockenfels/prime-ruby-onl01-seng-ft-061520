0a453fba69ad59b4eb6b0d7f0abe596918f68ddc
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-18 19:50:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,11 @@
 def prime?(num)
-  nums = 1..num
-  
+  nums = 2..num
+  nums.any { |val| num % val == 0 } 
+  if nums.length > 0
+    return false
+  else
+    return true
+  end
 end
 
 prime?(4)
