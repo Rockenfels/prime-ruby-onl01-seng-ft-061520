bb1d5005eee85870e62697ed20f972fb7724d907
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-18 19:53:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,7 @@
 def prime?(num)
   nums = (2..num).to_a
-  nums.any? { |val| num % val == 0 } 
-  if nums.length > 0
-    return false
-  else
-    return true
-  end
+  ans = nums.any? { |val| num % val == 0 } 
+  ans
 end
 
 prime?(4)
